863d4743962e74b0dce112026dd2fbc11e5af3a4
Allan Guerra <allan.guerra@puc-campinas.edu.br>
2019-11-28 16:39:28 -0300

    first commit

---
+++ src/test/java/com/cielo/api/desafiocielo/DesafioCieloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cielo.api.desafiocielo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCieloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
