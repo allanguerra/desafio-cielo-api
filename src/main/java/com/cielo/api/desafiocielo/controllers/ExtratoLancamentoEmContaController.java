83dcfbb95ac219571ec3ec2929b21bde80fa397f
Allan Guerra <allan.guerra19@gmail.com>
2019-12-02 22:07:02 -0300

    liberando crossorigin para a controller de extrato de lancamentos

--- src/main/java/com/cielo/api/desafiocielo/controllers/ExtratoLancamentoEmContaController.java
+++ src/main/java/com/cielo/api/desafiocielo/controllers/ExtratoLancamentoEmContaController.java
@@ -4,6 +4,7 @@ import com.cielo.api.desafiocielo.domain.dto.ExtratoLancamentoEmContaDTO;
 import com.cielo.api.desafiocielo.services.ExtratoLancamentoEmContaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.List;
  * Recebe e Responde requisições.
  * @author allanguerra
  */
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/extrato-lancamento")
 public class ExtratoLancamentoEmContaController {
